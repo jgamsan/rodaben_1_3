4984b975ce48a30899f844c82a52e6c22959213e
jose miguel <miguel.gamazo@galiclick.com>
2013-01-08 19:21:14 +0100

    Mas mejoras

--- app/helpers/spree/products_helper_decorator.rb
+++ app/helpers/spree/products_helper_decorator.rb
@@ -1,17 +1,19 @@
 Spree::ProductsHelper.class_eval do
   def eco_image(f)
+    fuel_options = Hash["A", "-14-55", "B", "-13-33", "C", "-13-11", "D", "-13+11", "E", "-13+33", "F", "-13+55", "G", "-13+77"]
+    wet_options = Hash["A", "+103-53", "B", "+103-31", "C", "+103-9", "D", "+103+13", "E", "+103+35", "F", "+103+56", "G", "+103+78"]
     image = MiniMagick::Image.open("#{Rails.root}/app/assets/images/base_etiqueta.jpg")
-    fuel = f.master.tire_fuel_consumption.name.downcase
-    wet = f.master.tire_wet_grip.name.downcase
+    fuel = f.master.tire_fuel_consumption.name
+    wet = f.master.tire_wet_grip.name
     noise_db = f.master.tire_rolling_noise_db
     noise_wave = f.master.tire_rolling_noise_wave
-    result = image.composite(MiniMagick::Image.open("#{Rails.root}/app/assets/images/#{fuel}.jpg"), "jpg") do |c|
+    result = image.composite(MiniMagick::Image.open("#{Rails.root}/app/assets/images/#{fuel.downcase}.jpg"), "jpg") do |c|
       c.gravity "center"
-      c.geometry "-13-33"
+      c.geometry fuel_options[fuel]
     end
-    result = result.composite(MiniMagick::Image.open("#{Rails.root}/app/assets/images/#{wet}.jpg", "jpg")) do |c|
+    result = result.composite(MiniMagick::Image.open("#{Rails.root}/app/assets/images/#{wet.downcase}.jpg", "jpg")) do |c|
       c.gravity "center"
-      c.geometry "+103+13"
+      c.geometry wet_options[wet]
     end
     result = result.composite(MiniMagick::Image.open("#{Rails.root}/app/assets/images/emision_ruido_#{noise_wave}.jpg", "jpg")) do |c|
       c.gravity "center"
