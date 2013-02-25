6c15e648da6123fcffb8fb29ac88e6348e62faad
jose miguel <miguel.gamazo@galiclick.com>
2013-02-25 21:39:15 +0100

    Mas mejoras

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,15 +1,15 @@
 every '45 02,14 * * 1-7', :roles => [:app] do
-  command "cd /media/disco_b/public_html/neumaticos/current && bundle exec rake RAILS_ENV=production products:to_rodaben_gane"
+  command 'cd /media/disco_b/public_html/neumaticos/current && bundle exec rake RAILS_ENV=production products:to_rodaben_gane'
 end
 every '15 04,14 * * 1-7', :roles => [:app] do
-  command "cd /media/disco_b/public_html/neumaticos/current && bundle exec rake RAILS_ENV=production products:to_rodaben_eurotyre"
+  command 'cd /media/disco_b/public_html/neumaticos/current && bundle exec rake RAILS_ENV=production products:to_rodaben_eurotyre'
 end
 every :day, :at => '5:00am', :roles => [:app] do
-  command "cd /media/disco_b/public_html/neumaticos/current && bundle exec rake RAILS_ENV=production products:clean_rodaben"
+  command 'cd /media/disco_b/public_html/neumaticos/current && bundle exec rake RAILS_ENV=production products:clean_rodaben'
 end
 every :day, :at => '2:00pm', :roles => [:app] do
-  command "backup perform --trigger rodaben_backup"
+  command 'backup perform --trigger rodaben_backup'
 end
 every :day, :at => '0:30am', :roles => [:app] do
-  command "backup perform --trigger rodaben_backup"
+  command 'backup perform --trigger rodaben_backup'
 end
