44fe040e8846f05a74352efd2cfca657ec0622f1
jose miguel <miguel.gamazo@galiclick.com>
2013-08-24 12:08:30 +0200

    Corregido error en schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,15 +1,15 @@
-every '45 02,14 * * 1-7', :roles => [:app] do
+every '45 02,14 * * 1-7' do
   command 'cd /media/disco_b/public_html/neumaticos/current && bundle exec rake RAILS_ENV=production products:to_rodaben_gane'
 end
-every '15 04,14 * * 1-7', :roles => [:app] do
+every '15 04,14 * * 1-7' do
   command 'cd /media/disco_b/public_html/neumaticos/current && bundle exec rake RAILS_ENV=production products:to_rodaben_eurotyre'
 end
-every :day, :at => '5:00am', :roles => [:app] do
+every :day, :at => '5:00am' do
   command 'cd /media/disco_b/public_html/neumaticos/current && bundle exec rake RAILS_ENV=production products:clean_rodaben'
 end
-every :day, :at => '2:00pm', :roles => [:app] do
+every :day, :at => '2:00pm' do
   command 'backup perform --trigger rodaben_backup'
 end
-every :day, :at => '0:30am', :roles => [:app] do
+every :day, :at => '0:30am' do
   command 'backup perform --trigger rodaben_backup'
 end
